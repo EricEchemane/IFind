98b2a0b62385f43cd215835bb38a74bacb01ad88
Eric Echemane <eechemane29@gmail.com>
2022-10-27 12:42:07 +0800

    navbar setup

---
+++ pages/developers.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Developers() {
+    return (
+        <div>Developers</div>
+    );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,6 @@
+import { Group, Paper, Title } from '@mantine/core';
 import Head from 'next/head';
+import Link from 'next/link';
 import React from 'react';
 
 export default function HomePage() {
@@ -7,5 +9,17 @@ export default function HomePage() {
     <Head>
       <title> iFind </title>
     </Head>
+
+    <Paper shadow={'lg'} p='1rem'>
+      <Group position='apart'>
+        <Title> iFind </Title>
+        <Group spacing={24}>
+          <Link href={'/'}> Home </Link>
+          <Link href={'/project'}> Project </Link>
+          <Link href={'/developers'}> Developers </Link>
+          <a href={'/docs/research.pdf'} target='_blank' rel='noreferrer'> Research </a>
+        </Group>
+      </Group>
+    </Paper>
   </>;
 }
