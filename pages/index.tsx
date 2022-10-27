79d4ee7d922e77cc570b4040156d720620114afd
Eric Echemane <eechemane29@gmail.com>
2022-10-27 12:56:53 +0800

    landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
+import { Button, Center, Stack, Title } from '@mantine/core';
 import Navbar from 'components/Navbar';
 import Head from 'next/head';
+import Link from 'next/link';
 
 import React from 'react';
 
@@ -11,5 +13,18 @@ export default function HomePage() {
     </Head>
 
     <Navbar />
+
+    <Stack style={{ width: 'min(1000px, 90vw', margin: '5rem auto' }}>
+      <Center>
+        <Stack spacing={0} align='center'>
+          <Title> FACIAL POINTS PREDICTION </Title>
+          <Title color={'#df3f00'}> OF MISSING PERSON </Title>
+          <Title> THROUGH IMAGE PROCESSING </Title>
+          <a href='/docs/research.pdf' rel='noreferrer' target={'_blank'}>
+            <Button size='lg' sx={{ marginTop: '3rem' }}> Read more </Button>
+          </a>
+        </Stack>
+      </Center>
+    </Stack>
   </>;
 }
