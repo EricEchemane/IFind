39dfd2777c447c868da202b882324562eac01cb8
Eric Echemane <eechemane29@gmail.com>
2022-10-27 14:02:12 +0800

    logo added in navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import React from 'react';
 import { Group, Paper, Title } from '@mantine/core';
 import Link from 'next/link';
@@ -5,7 +6,10 @@ import Link from 'next/link';
 export default function Navbar() {
     return <Paper shadow={'lg'} p='1rem' style={{ position: 'sticky', top: 0 }}>
         <Group position='apart'>
-            <Title> iFind </Title>
+            <Group spacing={8}>
+                <img src="/logo.png" alt="logo" width={30} style={{ filter: 'invert(100%)' }} />
+                <Title> iFind </Title>
+            </Group>
             <Group spacing={24}>
                 <Link href={'/'}> Home </Link>
                 <Link href={'/project'}> Project </Link>
