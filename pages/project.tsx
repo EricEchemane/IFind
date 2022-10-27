27bdfd6475a6e5bade082e8f76f6e1868884f393
Eric Echemane <eechemane29@gmail.com>
2022-10-27 12:34:15 +0800

    project route

---
+++ pages/project.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+import React from 'react';
+import * as tf from "@tensorflow/tfjs";
+
+export default function Project() {
+    return <>
+        <Head>
+            <title> Classification </title>
+        </Head>
+
+        <div>
+            Project
+        </div>
+    </>;
+}
